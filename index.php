a671e96b96a76fe3f4b0c261518053e290fcf679
@linkeefe <linkeefe@gmail.com>
2020-09-02 17:53:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -68,7 +68,7 @@
 
     <!-- Page Content -->
     <div class="container">
-      <div class="row">
+      <div class="row mb-2">
 
       <?php
       $string = file_get_contents("communities.json");
@@ -76,7 +76,7 @@
       
       foreach ($json_a as $key => $value) {
         ?>
-        <div class="col-lg-12 mb-2 p-4">
+        <div class="col-lg-12 p-4">
             <div class="card">
               <div class="card-body"> 
                 <h5 class="card-title"><?php echo $value['Name'] ?></h5>
