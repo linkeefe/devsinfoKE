87fc0c3ce883a93ceddd0e6c1a9c6727ab044560
@linkeefe <linkeefe@gmail.com>
2020-09-07 16:19:32 +0300

    Update about.php

--- about.php
+++ about.php
@@ -13,7 +13,18 @@
       <meta name="author" content="Lincoln Njogu, Technologist" />
       <title>Developer Communities Kenya</title>
       <!-- Bootstrap core CSS -->
-      <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="icon" type="image/png" href="ico.png"/>
+    
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-163468828-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-163468828-1');
+</script>
    </head>
    <body style="font-family: 'Courier New', Courier, monospace">
       <!-- Navigation -->
